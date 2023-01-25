a75956b3b79786fd894f2236b481a4b736d8d0f8
D3admous3 <43110723+VerbruggenD@users.noreply.github.com>
2023-01-25 18:07:43 +0100

    updated python consumer html generator

--- Html generator - Rest Python/Rest_consumer.py
+++ Html generator - Rest Python/Rest_consumer.py
@@ -2,5 +2,10 @@ import requests
 
 instructions = ['row', 'col', 'row', 'col', 'end']
 
-response = requests.post('http://127.0.0.1:5000/html', json={'instructions': instructions})
+response = requests.post('http://127.0.0.1:8000/html', json={'instructions': instructions})
 print(response.text)
+
+# Open the file in write mode
+with open('index.html', 'w') as f:
+#   Write the string to the file
+ f.write(response.text)
